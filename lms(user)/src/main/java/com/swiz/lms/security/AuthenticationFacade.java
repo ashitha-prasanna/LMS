43d5989a0efcad23d0b5e256bfb1b44ce9cc6bbb
ashitha-prasanna <ashitha.v@prasannatechnologies.com>
2025-04-23 15:13:23 +0530

    Delete lms(user) directory

--- lms(user)/src/main/java/com/swiz/lms/security/AuthenticationFacade.java
+++
@@ -1,8 +0,0 @@
-package com.swiz.lms.security;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface AuthenticationFacade {
-    UserDetails getCurrentUser();
-
-}
